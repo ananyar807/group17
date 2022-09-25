f15d857926d1b0eb3db1fcf8090bac8c091156fe
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2022-09-25 13:55:44 -0400

    added map

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions} from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MapView style={styles.map} 
+    initialRegion={{
+      latitude: 42.3398,
+      longitude: -71.0892,
+      latitudeDelta: 0.05,
+      longitudeDelta: 0.05
+    }}
+    zoomControlEnabled={true}
+    zoomEnabled={true}
+    scrollEnabled={true}
+    zoomTapEnabled={true}/>
   );
 }
 
@@ -16,5 +24,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
